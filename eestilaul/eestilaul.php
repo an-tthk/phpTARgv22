e063e90b81ca07c05b028121a9fa7162eeccb0a3
an-tthk <opilane@tthk.ee>
2023-09-26 18:24:08 +0300

    Update

--- eestilaul/eestilaul.php
+++ eestilaul/eestilaul.php
@@ -2,10 +2,19 @@
 require ('../baas/conf.php');
 global $yhendus;
 
-if (isset($_REQUEST["update"])) {
+if (isset($_REQUEST["lisapunkt"])) {
     $kask = $yhendus->prepare("UPDATE eestilaul set punktid=punktid+1 WHERE id=?;");
 
-    $kask->bind_param("i", $_REQUEST["update"]);
+    $kask->bind_param("i", $_REQUEST["lisapunkt"]);
+    $kask->execute();
+
+    header("Location: $_SERVER[PHP_SELF]");
+}
+
+if (isset($_REQUEST["kustutapunkt"])) {
+    $kask = $yhendus->prepare("UPDATE eestilaul set punktid=GREATEST(0,punktid-1) WHERE id=?;");
+
+    $kask->bind_param("i", $_REQUEST["kustutapunkt"]);
     $kask->execute();
 
     header("Location: $_SERVER[PHP_SELF]");
@@ -23,6 +32,24 @@ if (isset($_REQUEST["uuskomment"])) {
     header("Location: $_SERVER[PHP_SELF]");
 }
 
+if (isset($_REQUEST["kustutakommentaarid"])) {
+    $kask = $yhendus->prepare("UPDATE eestilaul set kommentaarid='' WHERE id=?;");
+
+    $kask->bind_param("i", $_REQUEST["kustutakommentaarid"]);
+    $kask->execute();
+
+    header("Location: $_SERVER[PHP_SELF]");
+}
+
+if (isset($_REQUEST["kustutaviimanekommentaar"])) {
+    $kask = $yhendus->prepare("UPDATE eestilaul set kommentaarid=REPLACE(kommentaarid, SUBSTRING_INDEX(kommentaarid, '\n', -2), '') WHERE id=?;");
+
+    $kask->bind_param("i", $_REQUEST["kustutaviimanekommentaar"]);
+    $kask->execute();
+
+    header("Location: $_SERVER[PHP_SELF]");
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -30,6 +57,7 @@ if (isset($_REQUEST["uuskomment"])) {
 <head>
     <meta charset="UTF-8">
     <title>Eesti TARgv22 laulukonkurss</title>
+    <link rel="stylesheet" type="text/css" href="../style/style.css">
 </head>
 <body>
 <h1>Eesti TARgv22 laulukonkurss</h1>
@@ -52,7 +80,8 @@ if (isset($_REQUEST["uuskomment"])) {
         echo "<td>".$laulunimi."</td>";
         echo "<td>".$laulja."</td>";
         echo "<td>".$punktid."</td>";
-        echo "<td><a href='?update=$id'>Lisa 1 punkt</a></td>";
+        echo "<td><a href='?lisapunkt=$id'>Lisa 1 punkt</a></td>";
+        echo "<td><a href='?kustutapunkt=$id'>Kustuta 1 punkt</a></td>";
         echo "<td>".nl2br(htmlspecialchars($kommentaarid))."</td>";
 
         echo "
@@ -64,6 +93,9 @@ if (isset($_REQUEST["uuskomment"])) {
                 </form>
             </td>
         ";
+
+        echo "<td><a href='?kustutakommentaarid=$id'>Kustuta kommentaarid</a></td>";
+        echo "<td><a href='?kustutaviimanekommentaar=$id'>Kustuta viimane kommentaar</a></td>";
         echo "</tr>";
     }
 ?>
