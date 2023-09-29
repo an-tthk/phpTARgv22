9fc1773967be74ef32f653ddc90e16525709a494
Nurkz <contact@nurkz.dev>
2023-09-29 16:59:51 +0300

    ylesanded

---
+++ content/ylesanded.php
@@ -0,0 +1,16 @@
+<?php
+
+function echobr ($str) {
+	echo $str . "<br/>";
+}
+
+echobr("Ülesanne 1");
+echobr("Iga ülesanne hakkab kommentaariga, kus on kirjas ülesande number, sinu nimi ja kuupäev");
+echobr("Programm väljastab kolme muutuja (nimi, sünniaasta, tähtkuju) väärtused ühe koodireaga. Kusjuures kõik on väljastades eraldi real. Vastus täislausega.");
+echobr("Väljasta järgnev lause: \"It’s My Life\" – Dr. Alban");
+echobr("\"Joonista\" järgmine pilt");
+
+echo "<hr/>";
+
+
+
